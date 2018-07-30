bd4579ad1321ed68f5baff66be5f732a58cff854
Bryan Silverio <bsn_12903@hotmail.com>
2018-07-30 16:58:04 -0500

    first commit

---
+++ AppLog/LogExceptions/Log/LogException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogExceptions.Log
+{
+    public class LogException
+    {
+        public static void Save(object obj, Exception e, string pathFile)
+        {
+            string fecha = DateTime.Now.ToString("yyyyMMdd");
+            string hora = DateTime.Now.ToString("HH:mm:ss");
+            string path = pathFile;
+
+            StreamWriter sw = new StreamWriter(path, true);
+
+            StackTrace stackTrace = new StackTrace();
+            sw.WriteLine(obj.GetType().FullName + " " + hora);
+            sw.WriteLine(stackTrace.GetFrame(1).GetMethod().Name + " - " + e.Message);
+            sw.WriteLine(e.InnerException);
+            sw.WriteLine("");
+
+            sw.Flush();
+            sw.Close();
+        }
+    }
+}
